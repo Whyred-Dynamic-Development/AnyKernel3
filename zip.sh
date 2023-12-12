8ffc6febe27054961b75260765f39ec0b87ef6d1
NopeNopeGuy <kingdutchisbad@gmail.com>
2023-12-12 19:27:24 +0530

    zip.sh: Fix path

--- zip.sh
+++ zip.sh
@@ -3,6 +3,6 @@ datenow=$(date +%Y\ %B\ %d)
 kernelname="San-Kernel-V4.19-Mesh-R1"
 echo $datenow
 echo $release
-sed -i "s/\(ini_set(\"kernel_name\", \s*\)\"[^\"]*\"/\1\"$kernelname\"/" test.txt
-sed -i "s/\(ini_set(\"kernel_date\", \s*\)\"[^\"]*\"/\1\"$datenow\"/" test.txt
+sed -i "s/\(ini_set(\"kernel_name\", \s*\)\"[^\"]*\"/\1\"$kernelname\"/" META-INF/com/google/android/aroma-config
+sed -i "s/\(ini_set(\"kernel_date\", \s*\)\"[^\"]*\"/\1\"$datenow\"/" META-INF/com/google/android/aroma-config
 zip -r9 $kernelname.zip * -x .git README.md *placeholder *zip zip.sh *txt
